f91bb71bde9b877c5cad5eada1aa5b2ea81dca1b
Takuya Mikami <mkmn52@gmail.com>
2014-09-22 21:39:42 +0900

    Topics Controllerのshow作成

--- test/functional/topics_controller_test.rb
+++ test/functional/topics_controller_test.rb
@@ -2,9 +2,20 @@ require 'test_helper'
 
 class TopicsControllerTest < ActionController::TestCase
   test "index" do
-    10.times { Factory(:topic) }
+    10.times { 
+      Factory(:topic)
+    }
     get :index
     assert_response :success
     assert_equal 10, assigns[:topics].count
+  end
+
+  test "show" do
+    topic = Factory(:topic_articles)
+
+    get :show, id: topic
+    assert_response :success
+    assert_equal 10, assigns[:topic].articles.count
+    assert_select ".article", 10
   end
 end
