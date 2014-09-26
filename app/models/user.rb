197c00d413babf1998a334fa75db7e17f399738c
Takuya Mikami <mkmn52@gmail.com>
2014-09-26 17:00:31 +0900

    User Modelの作成

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  attr_accessible :name, :password, :password_confirmation
+
+  validates :name,
+    presence: true,
+    length: { maximum: 32 },
+    uniqueness: { case_sensitive: false }
+  validates :password,
+    presence: true,
+    length: { minimum: 6 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140918070730) do
+ActiveRecord::Schema.define(:version => 20140926061256) do
 
   create_table "articles", :force => true do |t|
     t.string   "poster",     :null => false
@@ -27,5 +27,14 @@ ActiveRecord::Schema.define(:version => 20140918070730) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "name",            :null => false
+    t.string   "password_digest", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "users", ["name"], :name => "index_users_on_name", :unique => true
 
 end
