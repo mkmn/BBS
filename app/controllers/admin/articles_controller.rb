7fc9a37a3370392538cc5939f4342768aa0c723b
Takuya Mikami <mkmn52@gmail.com>
2014-09-27 01:41:02 +0900

    Admin::Articles Controllerの作成

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,25 @@
+class Admin::ArticlesController < Admin::Base
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @article = Article.find(params[:id])
+    @article.assign_attributes(params[:article])
+
+    if @article.save
+      redirect_to [:admin, @topic], notice: "投稿を更新しました"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to [:admin, @topic], notice: "投稿を削除しました"
+  end
+end
